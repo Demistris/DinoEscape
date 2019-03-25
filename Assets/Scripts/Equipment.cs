7526f0ef8dc2825acac89277472b1e8a5a08aab8
Kaja Więckowska <k.wieckowska66@gmail.com>
2019-03-25 23:19:43 +0100

    Unity project added

---
+++ Assets/Scripts/Equipment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equipment : MonoBehaviour
+{
+
+    public bool[] stuff;
+
+    void Start()
+    {
+        stuff = new bool[] { true, false, false, false, false };
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void AddToEq(short _id)
+    {
+        stuff[_id] = true;
+    }
+}
