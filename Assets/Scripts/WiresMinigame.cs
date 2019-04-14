eb46f12968e7e1ffb13c005bdb1fd77adee53f3c
Kaja Więckowska <k.wieckowska66@gmail.com>
2019-04-15 00:18:59 +0200

    Added lightning to wires

---
+++ Assets/Scripts/WiresMinigame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WiresMinigame : MonoBehaviour
+{
+    public Camera MainCamera;
+    public Equipment Eq;
+    public GameObject Arrows;
+
+    public void OnMouseDown()
+    {
+        MainCamera.transform.position = new Vector3(MainCamera.transform.position.x, MainCamera.transform.position.y + 20.25f, MainCamera.transform.position.z);
+        //Eq.ChangePosition(-40.50f);
+        UpdateArrowsVisibility();
+    }
+
+    private void UpdateArrowsVisibility()
+    {
+        Arrows.SetActive(false);
+    }
+}
